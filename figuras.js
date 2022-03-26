6780146c828f3eae2251724fda2c7f6d5872ced9
camlo01 <camilobeltran174@gmail.com>
2022-03-26 17:55:28 -0500

    se agrega boton calcular area de triangulo y estilos en cards y buttons

--- figuras.js
+++ figuras.js
@@ -7,18 +7,6 @@ function areaCuadrado(lado) {
   return lado * lado;
 }
 
-//Código del triángulo -----------------
-
-function perimetroTriangulo(lado, base) {
-  const ladosIrregurale = lado * 2;
-
-  return ladosIrregurale + base;
-}
-
-function areaTriangulo(base, altura) {
-  return (base * altura) / 2;
-}
-
 // //Código del círculo ------------
 //Circunferencia
 function calcularCircunferencia(diametro) {
@@ -30,6 +18,25 @@ function calcularCircunferencia(diametro) {
 function AreaCirculo(radio) {
   const PI = Math.PI;
   return PI * (radio * radio);
+}
+
+//Código del triángulo -----------------
+//Perímetro
+function perimetroTriangulo(lado, base) {
+  const ladosIrregurale = lado * 2;
+
+  return ladosIrregurale + base;
+}
+//área
+function areaTriangulo(base, altura) {
+  return (base * altura) / 2;
+}
+
+//Calcular altura
+function alturaTriangulo(lado, base) {
+  const mediaBase = base / 2;
+  let altura = Math.sqrt(lado * lado - mediaBase * mediaBase);
+  return altura;
 }
 
 //------------------------------------------------------
@@ -71,6 +78,16 @@ function calcularPerimetroTriangulo() {
   const base = inputBase.value;
   const perimetro = perimetroTriangulo(lado, base);
   alert("El perímetro del triángulo es: " + perimetro + " cm");
+}
+
+function calcularAlturaTriangulo() {
+  const inputLado = Document.getElementByID("inputLadoTriangulo");
+  const inputBase = Document.getElementByID("inputBaseTriangulo");
+  const lado = inputLado.Value;
+  const base = inputBase.Value;
+  const altura = alturaTriangulo(lado, base);
+
+  return (altura = mediaBase);
 }
 
 //CIRCUNFERENCIA
